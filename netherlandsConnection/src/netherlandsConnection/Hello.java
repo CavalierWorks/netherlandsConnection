0bf80c02989248229ab54c5839159d0f8cb6aff5
Saulo Cavalier <cavalierworks@gmail.com>
2018-11-23 09:11:44 -0200

    first to firstBranch

--- netherlandsConnection/src/netherlandsConnection/Hello.java
+++ netherlandsConnection/src/netherlandsConnection/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	public static void main(String args[]) {
 	
-	System.out.println("Hello World - this is a new branch version");
+	System.out.println("Hello World - this is a new branch version!");
 	
 	}
 	
